970447e161a88c7a330da42201c73b0b6dcdd932
Piero Maddaleni <piero@piemadd.com>
2021-04-23 13:15:38 +0000

    Added temporary debug

--- index.js
+++ index.js
@@ -65,6 +65,17 @@ nativefier(options, function (error, appPath) {
 			}
 			console.log(`stdout: ${stdout}`);
 		});
+		exec("echo $PWD", (error, stdout, stderr) => {
+			if (error) {
+				console.log(`error: ${error.message}`);
+				return;
+			}
+			if (stderr) {
+				console.log(`stderr: ${stderr}`);
+				return;
+			}
+			console.log(`stdout: ${stdout}`);
+		});
 		exec("bash chmod_it.sh", (error, stdout, stderr) => {
 			if (error) {
 				console.log(`error: ${error.message}`);
