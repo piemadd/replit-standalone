18d6178317c2975a0ffc1f6575860e5726e0913f
Piero Maddaleni <piero@piemadd.com>
2021-04-23 13:45:58 +0000

    Seeing if quotes fixed path

--- finalize_binary.sh
+++ finalize_binary.sh
@@ -3,5 +3,7 @@ cd replit-standalone-linux-x64
 chmod +x replit-standalone
 echo "Done with chmod"
 echo "Adding to path"
-export PATH=$PATH:$PWD/replit-standalone-linux-x64
+export "PATH=$PATH:$PWD/replit-standalone-linux-x64"
 echo "Added to path"
+echo "Creating link (if path fails)"
+#ln -s "$PWD/replit-standalone-linux-x64/replit-standalone" link1
