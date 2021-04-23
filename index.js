840fa279c5bfa777a6178f2cd9366a59a5af31c6
Piero Maddaleni <>
2021-04-23 13:20:46 +0000

    Listing files now

--- index.js
+++ index.js
@@ -76,6 +76,17 @@ nativefier(options, function (error, appPath) {
 			}
 			console.log(`stdout: ${stdout}`);
 		});
+		exec("ls", (error, stdout, stderr) => {
+			if (error) {
+				console.log(`error: ${error.message}`);
+				return;
+			}
+			if (stderr) {
+				console.log(`stderr: ${stderr}`);
+				return;
+			}
+			console.log(`stdout: ${stdout}`);
+		});
 		exec("bash chmod_it.sh", (error, stdout, stderr) => {
 			if (error) {
 				console.log(`error: ${error.message}`);
